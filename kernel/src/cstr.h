fe475098ac5cd231fe13726859f1faa933902f93
Absurdponcho <60100776+Absurdponcho@users.noreply.github.com>
2020-12-20 13:55:34 +1100

    episode 8

---
+++ kernel/src/cstr.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdint.h>
+
+const char* to_string(uint64_t value);
+const char* to_string(int64_t value);
+const char* to_hstring(uint64_t value);
+const char* to_hstring(uint32_t value);
+const char* to_hstring(uint16_t value);
+const char* to_hstring(uint8_t value);
+const char* to_string(double value, uint8_t decimalPlaces);
+const char* to_string(double value);
---
+++ kernel/src/efiMemory.cpp
@@ -0,0 +1,19 @@
+#include "efiMemory.h"
+
+const char* EFI_MEMORY_TYPE_STRINGS[] {
+
+    "EfiReservedMemoryType",
+    "EfiLoaderCode",
+    "EfiLoaderData",
+    "EfiBootServicesCode",
+    "EfiBootServicesData",
+    "EfiRuntimeServicesCode",
+    "EfiRuntimeServicesData",
+    "EfiConventionalMemory",
+    "EfiUnusableMemory",
+    "EfiACPIReclaimMemory",
+    "EfiACPIMemoryNVS",
+    "EfiMemoryMappedIO",
+    "EfiMemoryMappedIOPortSpace",
+    "EfiPalCode",
+};
